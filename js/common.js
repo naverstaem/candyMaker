454962522d2fdc97bf69cc734c59615524120aa4
fanian <rfanian@gmail.com>
2014-12-10 10:36:37 +0300

    add review section

--- js/common.js
+++ js/common.js
@@ -48,37 +48,44 @@ head.ready(function() {
 
 
     // Normalize Carousel Heights - pass in Bootstrap Carousel items.
-    //$.fn.carouselHeights = function() {
-    //
-    //    var items = $(this), //grab all slides
-    //        heights = [], //create empty array to store height values
-    //        tallest; //create variable to make note of the tallest slide
-    //
-    //    var normalizeHeights = function() {
-    //
-    //        items.each(function() { //add heights to array
-    //            heights.push($(this).height());
-    //        });
-    //        tallest = Math.max.apply(null, heights); //cache largest value
-    //        items.each(function() {
-    //            $(this).css('min-height',tallest + 'px');
-    //        });
-    //    };
-    //
-    //    normalizeHeights();
-    //
-    //    $(window).on('resize orientationchange', function () {
-    //        //reset vars
-    //        tallest = 0;
-    //        heights.length = 0;
-    //
-    //        items.each(function() {
-    //            $(this).css('min-height','0'); //reset min-height
-    //        });
-    //        normalizeHeights(); //run it again
-    //    });
-    //
-    //};
+    $.fn.carouselHeights = function() {
+
+        var items = $(this), //grab all slides
+            heights = [], //create empty array to store height values
+            tallest; //create variable to make note of the tallest slide
+
+        var normalizeHeights = function() {
+
+            items.each(function() { //add heights to array
+                heights.push($(this).height());
+            });
+            tallest = Math.max.apply(null, heights); //cache largest value
+            items.each(function() {
+                $(this).css('min-height',tallest + 'px');
+            });
+        };
+
+        normalizeHeights();
+
+        $(window).on('resize orientationchange', function () {
+            //reset vars
+            tallest = 0;
+            heights.length = 0;
+
+            items.each(function() {
+                $(this).css('min-height','0'); //reset min-height
+            });
+            normalizeHeights(); //run it again
+        });
+
+    };
+    jQuery(function($){
+
+        $(window).on('load', function(){
+            $('#carousel .item').carouselHeights();
+        });
+
+    });
 
     $('#form1').validate();
     $('#form2').validate();
